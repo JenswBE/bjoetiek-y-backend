7bcb62a232d8aa2f154db12710981bcf2a858f57
Jens Willemsens <jens@jensw.be>
2020-12-27 09:59:11 +0100

    Set gen_random_uuid as DEFAULT for uuid PK's

--- migrations/2020-12-23-185821_initial/up.sql
+++ migrations/2020-12-23-185821_initial/up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE manufacturers (
-    id uuid PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     name text NOT NULL,
     website_url text NOT NULL,
     logo_url text NOT NULL
 );
 
 CREATE TABLE products (
-    id uuid PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name text NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE products (
 SELECT diesel_manage_updated_at('products');
 
 CREATE TABLE categories (
-    id uuid PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     name text NOT NULL,
     description text NOT NULL,
     image_url text NOT NULL,
