1f5a54b55f27fa4dcab6686d3482cb07a6182225
Jens Willemsens <jens@jensw.be>
2020-12-30 15:17:50 +0100

    Fix .env reading

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bjoetiek;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
     let config = bjoetiek::models::Config::from_env();
     bjoetiek::run(config).await
 }
