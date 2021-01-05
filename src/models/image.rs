8608671c22edb25b77b6a563910231da44f09550
Jens Willemsens <jens@jensw.be>
2021-01-05 22:10:49 +0100

    Update docs

--- src/models/image.rs
+++ src/models/image.rs
@@ -2,9 +2,6 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct ThumbnailRequest {
-    /// UUID of the owner (product, category, ...)
-    pub id: uuid::Uuid,
-
     /// Max width of thumbnail
     pub width: i32,
 
